9b2e5a58e9f00f7497b202e686354519eb3f0b33
SrZafkiell <75232612+SrZafkiell@users.noreply.github.com>
2022-11-21 16:54:27 -0500

    Add files via upload

---
+++ keys.py
@@ -0,0 +1,10 @@
+# Author: Daniel Alejandro Ceron Rayo
+# Software development student at Universidad del valle Cali-Colombia / 1st semester
+# Mail: daniel.rayo@correounivalle.edu.co
+
+botToken = 'BOT TOKEN'
+twitchHeaders = {
+        "Authorization": "Bearer ",
+        "Client-Id": ""
+    }
+twitchApiUrl = 'https://api.twitch.tv/helix/'
